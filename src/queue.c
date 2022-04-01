d443727ea66fee64ec1710e2ecc97ed101a15848
Loïc TRUPIN <loic.trupin@enseirb-matmeca.fr>
2022-04-01 12:01:48 +0200

    new queue.h

--- src/queue.c
+++ src/queue.c
@@ -1,3 +1,22 @@
 #include "queue.h"
 
-struct node sentinel;
+struct node sentinel = {NULL, NULL};
+
+int queue__add(struct queue* queue, void* x)
+{
+	struct node* new_node = malloc(sizeof(struct node));
+	if (!new_node)
+		return -1;
+
+	new_node->next = NULL;
+	new_node->value = x;
+
+	queue->end->next = new_node;
+
+	return 0;
+}
+
+void* queue__pop(struct queue* queue)
+{
+	void* return_value;
+}
