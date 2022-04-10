133be776154bb30dbaf417c4fa68cb412d4eaa19
Loïc TRUPIN <loic.trupin@enseirb-matmeca.fr>
2022-04-10 20:31:37 +0200

    Better queues

--- src/queue.c
+++ src/queue.c
@@ -1,15 +1,17 @@
 #include "queue.h"
+#include "thread.h"
 
 #include <stdlib.h>
 
-// WARN : Éviter la file vide et initialiser avec une valeur (toujours au moins le main)
-
+#ifndef BETTER_QUEUE
 struct node sentinel = {NULL, NULL};
 
-void queue__init(struct queue* queue)
+void queue__init(struct queue* queue, void* base)
 {
-	queue->top = &sentinel;
-	queue->end = &sentinel;
+	queue->base.next = &sentinel;
+	queue->base.value = base;
+	queue->top = &queue->base;
+	queue->end = &queue->base;
 }
 
 int queue__add(struct queue* queue, void* x)
@@ -20,13 +22,8 @@ int queue__add(struct queue* queue, void* x)
 	new_node->next = &sentinel;
 	new_node->value = x;
 
-	if (queue->top == &sentinel) {
-		queue->top = new_node;
-		queue->end = new_node;
-	} else {
-		queue->end->next = new_node;
-		queue->end = new_node;
-	}
+	queue->end->next = new_node;
+	queue->end = new_node;
 
 	return 0;
 }
@@ -34,11 +31,6 @@ int queue__add(struct queue* queue, void* x)
 void* queue__top(struct queue* queue)
 {
 	return queue->top->value;
-}
-
-int queue__is_empty(struct queue* queue)
-{
-	return queue->top == &sentinel || queue->end == &sentinel;
 }
 
 void* queue__pop(struct queue* queue)
@@ -50,16 +42,14 @@ void* queue__pop(struct queue* queue)
 
 	free(top_node);
 
-	if (queue->top == &sentinel)
-		queue->end = &sentinel;
+	// assert(queue->top != &sentinel);
 
 	return return_value;
 }
 
-int queue_roll(struct queue* queue)
+int queue__roll(struct queue* queue)
 {
-	if (queue->top == &sentinel)
-		return 0;
+	// assert(queue->top != &sentinel);
 
 	queue->end->next = queue->top;
 
@@ -70,3 +60,99 @@ int queue_roll(struct queue* queue)
 
 	return 0;
 }
+
+void queue__release(struct queue* queue)
+{
+	(void)queue;
+}
+
+#else
+
+	#define FIRST_SIZE 10
+
+struct node sentinel = {NULL, NULL};
+
+void queue__init(struct queue* queue, void* base)
+{
+	queue->pool = malloc(sizeof(struct node) * FIRST_SIZE);
+	queue->pool[0].next = &sentinel;
+	queue->pool[0].value = base;
+	queue->top = &queue->pool[0];
+	queue->end = &queue->pool[0];
+	queue->empty = NULL;
+	queue->last_empty = &queue->pool[1];
+	queue->size = FIRST_SIZE;
+	queue->free_space = FIRST_SIZE - 1;
+}
+
+int queue__add(struct queue* queue, void* x)
+{
+	if (queue->empty) {
+		struct node* element = queue->empty;
+		queue->empty = queue->empty->next;
+		queue->end->next = element;
+		queue->end = element;
+		element->value = x;
+		element->next = &sentinel;
+		queue->free_space -= 1;
+		return 0;
+	}
+
+	if (queue->free_space == 0) {
+		queue->free_space = queue->size - 1;
+		queue->pool = realloc(queue->pool, 2 * queue->size * sizeof(struct node));
+		queue->last_empty = queue->pool + queue->size;
+		queue->size *= 2;
+	}
+
+	struct node* element = queue->last_empty;
+	queue->last_empty += 1;
+	queue->end->next = element;
+	queue->end = element;
+	element->value = x;
+	element->next = &sentinel;
+
+	return 0;
+}
+
+void* queue__top(struct queue* queue)
+{
+	return queue->top->value;
+}
+
+void* queue__pop(struct queue* queue)
+{
+	struct node* top_node = queue->top;
+	queue->top = top_node->next;
+
+	void* return_value = top_node->value;
+	top_node->value = NULL;
+
+	top_node->next = queue->empty;
+	queue->empty = top_node;
+
+	// assert(queue->top != &sentinel);
+
+	return return_value;
+}
+
+int queue__roll(struct queue* queue)
+{
+	// assert(queue->top != &sentinel);
+
+	queue->end->next = queue->top;
+
+	queue->top = queue->top->next;
+
+	queue->end = queue->end->next;
+	queue->end->next = &sentinel;
+
+	return 0;
+}
+
+void queue__release(struct queue* queue)
+{
+	free(queue->pool);
+}
+
+#endif
--- src/queue.h
+++ src/queue.h
@@ -8,14 +8,24 @@ struct node {
 	struct node* next;
 };
 
+#ifndef BETTER_QUEUE
 struct queue {
 	struct node *top, *end;
+	struct node base; // Node which won't be removed (for main thread)
 };
+#else
+struct queue {
+	struct node *top, *end;
+	struct node* pool;
+	struct node *empty, *last_empty;
+	size_t size, free_space;
+};
+#endif
 
 /* Fails if return value is -1 */
 
 // Init and allocate memory
-void queue__init(struct queue* queue);
+void queue__init(struct queue* queue, void* base);
 
 // Add at the end and reallocate more memory if needed
 int queue__add(struct queue* queue, void* x);
@@ -26,12 +36,10 @@ void* queue__pop(struct queue* queue);
 // Return top element
 void* queue__top(struct queue* queue);
 
-// Check if empty
-int queue__is_empty(struct queue* queue);
-
 // Add and pop
-int queue_roll(struct queue* queue);
+int queue__roll(struct queue* queue);
 
-// TODO : Queue release
+// Releases queue resources (if needed)
+void queue__release(struct queue* queue);
 
 #endif // __QUEUE_H__
