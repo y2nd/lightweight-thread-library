b39baae5d99381eaed4bc8559a0080e6aad4c42e
Loïc TRUPIN <loic.trupin@enseirb-matmeca.fr>
2022-04-10 19:06:55 +0200

    main.c au bon endroit

--- tst/main.c
+++ tst/main.c
@@ -7,7 +7,7 @@
 
 void* function1(void* arg)
 {
-	printf("Function %" PRIuPTR "\n", (intptr_t)arg);
+	// printf("Function %" PRIuPTR "\n", (intptr_t)arg);
 	thread_yield();
 	return (void*)(10 * (intptr_t)arg);
 }
@@ -16,21 +16,22 @@ int main()
 {
 	thread_yield();
 
-	printf("%p\n", thread_self());
+	// printf("%p\n", thread_self());
 
 	thread_t thread, thread2;
 	thread_create(&thread, function1, (void*)1);
 	thread_create(&thread2, function1, (void*)2);
 
 	thread_yield();
-	thread_yield();
 
 	void* retval;
 	thread_join(thread2, &retval);
-	printf("Function2 %" PRIuPTR "\n", (intptr_t)retval);
+	// printf("Function2 %" PRIuPTR "\n", (intptr_t)retval);
 
 	thread_join(thread, &retval);
-	printf("Function1 %" PRIuPTR "\n", (intptr_t)retval);
+	// printf("Function1 %" PRIuPTR "\n", (intptr_t)retval);
+
+	thread_yield();
 
 	return EXIT_SUCCESS;
 }
